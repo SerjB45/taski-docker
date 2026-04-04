89503475813f9746c7174b48187daa0014b41dca
Serj Bakumec <sbakumtsev@yandex.ru>
2026-04-04 21:02:07 +0300

    correct setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.225.31', '127.0.0.1', 'localhost', 'taskiyabs.hopto.org']
+ALLOWED_HOSTS = ['95.25.105.106', '127.0.0.1', 'localhost', 'taskiyabs.hopto.org']
 
 
 # Application definition
